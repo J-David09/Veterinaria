ccb62e4575a615a135e05e10321b1a3187c74111
jduribe <jduribe@softcaribbean.com>
2024-03-12 11:26:23 -0500

    Agregar componentes en front, cambios en el home

--- frontend-application/src/app/app-routing.module.ts
+++ frontend-application/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ClientesComponent } from './content/clientes/clientes.component';
+import { MascotasComponent } from './content/mascotas/mascotas.component';
+import { MedicamentosComponent } from './content/medicamentos/medicamentos.component';
 
 const routes: Routes = [
   {path: 'login',  component: LoginComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'mascotas', component: MascotasComponent},
+  {path: 'medicamentos', component: MedicamentosComponent},
 ];
 
 @NgModule({
--- frontend-application/src/app/app.module.ts
+++ frontend-application/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ClientesComponent } from './content/clientes/clientes.component';
+import { MascotasComponent } from './content/mascotas/mascotas.component';
+import { MedicamentosComponent } from './content/medicamentos/medicamentos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    ClientesComponent,
+    MascotasComponent,
+    MedicamentosComponent
   ],
   imports: [
     BrowserModule,
