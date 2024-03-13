12178abf1fa4a9526e9f0be678614ea183f8c4ca
Simon <simon_sanchez82192@elpoli.edu.co>
2024-03-12 21:03:25 -0500

    Avances en front de clientes e inicializacion de modales.

--- frontend-application/src/app/app.component.ts
+++ frontend-application/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,22 +8,38 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'frontend-application';
+  isClients: boolean = false;
+  isPets: boolean = false;
+  isFarmacy: boolean = false;  
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  isLoginPage(): boolean {
+  isLoginPage(): boolean {    
     return this.router.url === '/login';
   }
 
-  navigateMedicamentos () {
+  navigateMedicamentos() {
     this.router.navigate(["/medicamentos"]);
+    this.isClients = false;
+    this.isPets = false;
+    this.isFarmacy = true; 
   }
 
-  navigateMascotas () {
+  navigateMascotas() {
     this.router.navigate(["/mascotas"]);
+    this.isClients = false;
+    this.isPets = true;
+    this.isFarmacy = false; 
   }
 
-  navigateClientes () {
+  navigateClientes() {
     this.router.navigate(["/clientes"]);
+    this.isClients = true;
+    this.isPets = false;
+    this.isFarmacy = false; 
+  }
+
+  navigateHome() {
+    this.router.navigate([""]);
   }
 }
--- frontend-application/src/app/content/clientes/clientes.component.ts
+++ frontend-application/src/app/content/clientes/clientes.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./clientes.component.scss']
 })
 export class ClientesComponent {
+  isModal : boolean = false;
 
+  openModal(){
+    this.isModal = true;
+  }
+
+  closeModal(){
+    this.isModal = false;
+  }  
 }
