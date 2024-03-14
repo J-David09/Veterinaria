971d37b5cbccc42c24128796f6d89f73bdcbfd2d
jduribe <jduribe@softcaribbean.com>
2024-03-14 17:26:29 -0500

    Crud de mascotas en frontend completado

--- frontend-application/src/app/content/mascotas/mascotas.component.ts
+++ frontend-application/src/app/content/mascotas/mascotas.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Mascota } from 'src/app/interfaces/mascota';
+import { MascotaService } from 'src/app/services/mascota.service';
 
 @Component({
   selector: 'app-mascotas',
@@ -6,5 +8,102 @@ import { Component } from '@angular/core';
   styleUrls: ['./mascotas.component.scss']
 })
 export class MascotasComponent {
+  isAdd:boolean = false;;
+  isInfo:boolean = false;
+  isEdit:boolean = false;
+  isDelete:boolean = false;
 
+  mascotas: Mascota[] = [];
+
+  currentMascota : Mascota = {
+    id : 0,
+    nombre : "",
+    edad: 0,
+    idCliente: 0,
+    peso: 0,
+    raza: ""
+  }
+
+  constructor(private mascotaService: MascotaService) { }
+
+  ngOnInit() {
+    this.consultarUsuarios();
+  }
+
+  consultarUsuarios () {
+    this.mascotaService.getAll().subscribe(response => {
+      this.mascotas = response.data;
+      console.log(this.mascotas);
+    },
+    error => {
+      alert("Error al consultar usuarios");
+    })
+  }
+
+  editarUsuario (){
+    this.mascotaService.editUser(this.currentMascota.id, this.currentMascota).subscribe(response => {
+      this.consultarUsuarios;
+      this.closeModal();
+    },
+    error => {
+      alert("Error al actualizar usuario");
+      this.closeModal;
+    })
+  }
+
+  addUser (){
+    this.mascotaService.addUser(this.currentMascota).subscribe(response => {
+      this.consultarUsuarios();
+      this.closeModal();
+    },
+    error => {
+      alert("Error al crear el usuario");
+      this.closeModal;
+    })
+  }
+
+  delete () {
+    this.mascotaService.delete(this.currentMascota.id).subscribe(response => {
+      this.consultarUsuarios();
+      this.closeModal();
+    },
+    error => {
+      alert("Error al eliminar cliente");
+      this.closeModal();
+    })
+  }
+
+  openAddForm() {
+    this.isAdd = true;
+  }
+
+  openInfoForm(mascota: Mascota) {
+    this.isInfo = true;
+    this.currentMascota = mascota
+  }
+
+  openEditForm(mascota: Mascota) {
+    this.isEdit = true;
+    this.currentMascota = mascota
+  }
+
+  openDeleteForm(mascota: Mascota) {
+    this.isDelete = true;
+    this.currentMascota = mascota
+  }
+
+  closeModal() {    
+    this.isAdd = false;
+    this.isInfo = false;
+    this.isEdit = false;
+    this.isDelete = false;
+    this.currentMascota =  {
+      id : 0,
+      nombre : "",
+      edad: 0,
+      idCliente: 0,
+      peso: 0,
+      raza: ""
+    }
+  }
 }
---
+++ frontend-application/src/app/services/mascota.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from '../environments/environment';
+import { Mascota } from '../interfaces/mascota';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MascotaService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll (): Observable<any> {
+    return this.http.get<any>(environment.urlBaseApi + "mascota/getAll", {});
+  }
+
+  editUser (id: number, mascota:Mascota): Observable<any> {
+    return this.http.post<any>(environment.urlBaseApi + "mascota/update/" + id, mascota);
+  }
+
+  addUser (mascota:Mascota): Observable<any> {
+    return this.http.post<any>(environment.urlBaseApi + "mascota/create", mascota);
+  }
+
+  delete (id: number):  Observable<any> {
+    return this.http.delete<any>(environment.urlBaseApi + "mascota/delete/" + id);
+  }
+}
